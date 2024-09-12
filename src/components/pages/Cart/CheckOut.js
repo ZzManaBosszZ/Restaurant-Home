7785a841762458d6e450c31a5aa3b562c12adc4a
honghanh426417 <114908283+honghanh426417@users.noreply.github.com>
2024-09-12 10:35:54 +0700

    add css paypal

--- src/components/pages/Cart/CheckOut.js
+++ src/components/pages/Cart/CheckOut.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css'; // Import CSS của React-Toastify
+import "react-toastify/dist/ReactToastify.css"; // Import CSS của React-Toastify
 import api from "../../../services/api";
 import url from "../../../services/url";
 import { getAccessToken } from "../../../utils/auth";
@@ -9,11 +9,11 @@ import LayoutPages from "../../layouts/LayoutPage";
 import BreadCrumb from "../../layouts/BreadCrumb";
 import config from "../../../config";
 import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js"; // Import PayPal SDK
-import '../../../public/css/checkout.css';
+import "../../../public/css/checkout.css";
 
 function CheckOut() {
   const [customerInfo, setCustomerInfo] = useState({});
-  const [paymentDetails, setPaymentDetails] = useState('');
+  const [paymentDetails, setPaymentDetails] = useState("");
   const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
 
@@ -22,7 +22,9 @@ function CheckOut() {
   useEffect(() => {
     const loadProfile = async () => {
       try {
-        const response = await api.get(url.AUTH.PROFILE, { headers: { Authorization: `Bearer ${getAccessToken()}` } });
+        const response = await api.get(url.AUTH.PROFILE, {
+          headers: { Authorization: `Bearer ${getAccessToken()}` }
+        });
         setCustomerInfo(response.data.data);
       } catch (error) {
         console.error("Error loading profile:", error);
@@ -30,9 +32,12 @@ function CheckOut() {
     };
 
     const loadCartItems = () => {
-      const cart = JSON.parse(localStorage.getItem('selectedCartItems')) || [];
+      const cart = JSON.parse(localStorage.getItem("selectedCartItems")) || [];
       setCartItems(cart);
-      const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+      const total = cart.reduce(
+        (sum, item) => sum + item.price * item.quantity,
+        0
+      );
       setTotalPrice(total);
     };
 
@@ -47,11 +52,14 @@ function CheckOut() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setCustomerInfo(prevInfo => ({ ...prevInfo, [name]: value }));
+    setCustomerInfo((prevInfo) => ({ ...prevInfo, [name]: value }));
   };
 
   const handlePaymentMethodChange = (e) => {
-    setCustomerInfo(prevInfo => ({ ...prevInfo, paymentMethod: e.target.value }));
+    setCustomerInfo((prevInfo) => ({
+      ...prevInfo,
+      paymentMethod: e.target.value
+    }));
   };
 
   const handlePaymentDetailsChange = (e) => {
@@ -61,47 +69,61 @@ function CheckOut() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const selectedCartItems = JSON.parse(localStorage.getItem('selectedCartItems')) || [];
+    const selectedCartItems =
+      JSON.parse(localStorage.getItem("selectedCartItems")) || [];
 
     const createOrderPayload = {
       discount: customerInfo.discount || 0,
-      foodQuantities: selectedCartItems.map(item => ({
+      foodQuantities: selectedCartItems.map((item) => ({
         foodId: item.id,
         quantity: item.quantity
-      })),
+      }))
       // paymentMethod: selectedPaymentMethod
     };
 
     try {
-      const orderResponse = await api.post(url.ORDER.CREATE, createOrderPayload, {
-        headers: {
-          Authorization: `Bearer ${getAccessToken()}`,
-          'Content-Type': 'application/json'
+      const orderResponse = await api.post(
+        url.ORDER.CREATE,
+        createOrderPayload,
+        {
+          headers: {
+            Authorization: `Bearer ${getAccessToken()}`,
+            "Content-Type": "application/json"
+          }
         }
-      });
+      );
 
       if (orderResponse.status === 201) {
-        toast.success('Your order has been created successfully!');
+        toast.success("Your order has been created successfully!");
 
         // Remove selected items from local storage cart
-        let cart = JSON.parse(localStorage.getItem('cart')) || [];
-        cart = cart.filter(item => !selectedCartItems.some(selectedItem => selectedItem.id === item.id));
-        localStorage.setItem('cart', JSON.stringify(cart));
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+        cart = cart.filter(
+          (item) =>
+            !selectedCartItems.some(
+              (selectedItem) => selectedItem.id === item.id
+            )
+        );
+        localStorage.setItem("cart", JSON.stringify(cart));
 
         // Clear selected items from local storage
-        localStorage.removeItem('selectedCartItems');
+        localStorage.removeItem("selectedCartItems");
 
         setTimeout(() => {
           navigate(config.routes.order); // Navigate to order history page
         }, 3000);
       } else {
         const errorText = await orderResponse.text();
-        console.error('Error creating order:', errorText);
-        toast.error('There was an error creating your order. Please try again.');
+        console.error("Error creating order:", errorText);
+        toast.error(
+          "There was an error creating your order. Please try again."
+        );
       }
     } catch (error) {
-      console.error('Network error:', error);
-      toast.error('There was a problem connecting to the server. Please try again.');
+      console.error("Network error:", error);
+      toast.error(
+        "There was a problem connecting to the server. Please try again."
+      );
     }
   };
 
@@ -114,16 +136,20 @@ function CheckOut() {
   const handlePayPalSuccess = async (details) => {
     try {
       // Call your backend to finalize the order after payment
-      await api.post(url.ORDER.CREATE, { orderId: details.orderID }, {
-        headers: {
-          Authorization: `Bearer ${getAccessToken()}`,
-          'Content-Type': 'application/json'
+      await api.post(
+        url.ORDER.CREATE,
+        { orderId: details.orderID },
+        {
+          headers: {
+            Authorization: `Bearer ${getAccessToken()}`,
+            "Content-Type": "application/json"
+          }
         }
-      });
-      toast.success('Payment successful!');
+      );
+      toast.success("Payment successful!");
       navigate(config.routes.order); // Navigate to order history page
     } catch (error) {
-      toast.error('There was an error finalizing your order.');
+      toast.error("There was an error finalizing your order.");
     }
   };
 
@@ -140,97 +166,108 @@ function CheckOut() {
                   type="text"
                   id="fullName"
                   name="fullName"
-                  value={customerInfo.fullName || ''}
+                  value={customerInfo.fullName || ""}
                   onChange={handleChange}
                   required
                 />
               </div>
-
               <div className="form-group">
                 <label htmlFor="phone">Phone Number</label>
                 <input
                   type="text"
                   id="phone"
                   name="phone"
-                  value={customerInfo.phone || ''}
+                  value={customerInfo.phone || ""}
                   onChange={handleChange}
                   required
                 />
               </div>
-
               <div className="form-group">
                 <label htmlFor="address">Address</label>
                 <input
                   type="text"
                   id="address"
                   name="address"
-                  value={customerInfo.address || ''}
+                  value={customerInfo.address || ""}
                   onChange={handleChange}
                   required
                 />
               </div>
-
               <div className="form-group">
                 <label>Payment Method</label>
                 <select
                   name="paymentMethod"
-                  value={customerInfo.paymentMethod || ''}
+                  value={customerInfo.paymentMethod || ""}
                   onChange={handlePaymentMethodChange}
                 >
                   <option value="card">Credit/Debit Card</option>
                   <option value="bank">Bank Transfer</option>
-                  <option value="paypal">PayPal</option> {/* Added PayPal option */}
+                  <option value="paypal">PayPal</option>{" "}
+                  {/* Added PayPal option */}
                 </select>
               </div>
+              {customerInfo.paymentMethod === "card" ||
+              customerInfo.paymentMethod === "bank" ? (
+                <div className="payment-details-container">
+                  {customerInfo.paymentMethod === "card" && (
+                    <div className="form-group">
+                      <label htmlFor="paymentDetails">Card Details</label>
+                      <input
+                        type="text"
+                        id="paymentDetails"
+                        value={paymentDetails}
+                        onChange={handlePaymentDetailsChange}
+                        required
+                      />
+                    </div>
+                  )}
 
-              {customerInfo.paymentMethod === 'card' && (
-                <div className="form-group">
-                  <label htmlFor="paymentDetails">Card Details</label>
-                  <input
-                    type="text"
-                    id="paymentDetails"
-                    value={paymentDetails}
-                    onChange={handlePaymentDetailsChange}
-                    required
-                  />
+                  {customerInfo.paymentMethod === "bank" && (
+                    <div className="form-group">
+                      <label htmlFor="paymentDetails">
+                        Bank Transfer Instructions
+                      </label>
+                      <textarea
+                        id="paymentDetails"
+                        value={paymentDetails}
+                        onChange={handlePaymentDetailsChange}
+                        required
+                      ></textarea>
+                    </div>
+                  )}
                 </div>
-              )}
-
-              {customerInfo.paymentMethod === 'bank' && (
-                <div className="form-group">
-                  <label htmlFor="paymentDetails">Bank Transfer Instructions</label>
-                  <textarea
-                    id="paymentDetails"
-                    value={paymentDetails}
-                    onChange={handlePaymentDetailsChange}
-                    required
-                  ></textarea>
-                </div>
-              )}
-
-              <div className="order-summary">
+              ) : null}
+              <div className="order-summary_checkout">
                 <h3>Order Summary</h3>
                 <ul>
-                  {cartItems.filter(item => JSON.parse(localStorage.getItem('selectedCartItems') || []).some(selectedItem => selectedItem.id === item.id)).map(item => (
-                    <li key={item.id}>
-                      {item.name} x {item.quantity} - ${item.price * item.quantity}
-                    </li>
-                  ))}
+                  {cartItems
+                    .filter((item) =>
+                      JSON.parse(
+                        localStorage.getItem("selectedCartItems") || []
+                      ).some((selectedItem) => selectedItem.id === item.id)
+                    )
+                    .map((item) => (
+                      <li key={item.id}>
+                        {item.name} x {item.quantity} - $
+                        {item.price * item.quantity}
+                      </li>
+                    ))}
                 </ul>
                 <p>Total: ${totalPrice}</p>
               </div>
-              
-              {customerInfo.paymentMethod === 'paypal' && (
+              {customerInfo.paymentMethod === "paypal" && (
                 <div className="form-group">
                   <PayPalScriptProvider options={paypalOptions}>
                     <PayPalButtons
                       createOrder={(data, actions) => {
                         return actions.order.create({
-                          purchase_units: [{
-                            amount: {
-                              value: totalPrice.toFixed(2),
-                            },
-                          }],
+                          purchase_units: [
+                            {
+                              amount: {
+                                value: totalPrice.toFixed(2)
+                              }
+                            }
+                          ]
                         });
                       }}
                       onApprove={async (data, actions) => {
@@ -241,7 +278,13 @@ function CheckOut() {
                   </PayPalScriptProvider>
                 </div>
               )}
-              {customerInfo.paymentMethod !== 'paypal' && <button type="submit" className="btn btn-primary">Place Order</button>}
+              {customerInfo.paymentMethod !== "paypal" && (
+                <div className="button-container">
+                  <button type="submit" className="btn btn-primary">
+                    Place Order
+                  </button>
+                </div>
+              )}{" "}
             </form>
           </div>
         </div>
