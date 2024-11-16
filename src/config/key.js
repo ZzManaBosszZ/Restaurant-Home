88b16ed2affbe7e5f03c63aad77e02e6a092f077
Phuong <phuongphuong262204@gmail.com>
2024-11-16 09:53:48 +0700

    Update key.js

--- src/config/key.js
+++ src/config/key.js
@@ -1,5 +1,5 @@
 const key = {
-    BASE_API_URL: "http://localhost:8083/api/v1/",
+    BASE_API_URL: "http://localhost:8080/api/v1/",
     PAYPAL_CLIENT_ID: "Ac0ZDOq8PpTa0k1bcyliZWmLxLUqqmwZ2S-xtkazKKn_t1HWmZSE7kU9QyH92Jvk1_gWD6LXky-A6c-z",
 };
 
